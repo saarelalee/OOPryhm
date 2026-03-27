cb5dc6379da4f5dd2df57865ddb90465b1c39daa
Maarek <maarek.vettik@ut.ee>
2026-03-27 14:29:02 +0200

    Mängulauakoostamine ja kontrollimine

--- src/MängulauaKontrollija.java
+++ src/MängulauaKontrollija.java
@@ -1,2 +1,22 @@
 public class MängulauaKontrollija {
+
+    // Hetkel ainult proovimiseks
+    public void main() {
+        MängulauaKoostaja m = new MängulauaKoostaja(5,5);
+        char[][] maatriks = m.koosta();
+        System.out.println(kasSobib(maatriks));
+        System.out.println("---");
+
+        for (char[] maatrik : maatriks) {
+            System.out.println(maatrik);
+        }
+    }
+
+    public boolean kasSobib(char[][] mänguLaud) {
+        int pikkus = mänguLaud.length;
+        int laius = mänguLaud[0].length;
+
+        if ((pikkus * laius) % 2 == 0) return true;
+        else return false;
+    }
 }
--- src/MängulauaKoostaja.java
+++ src/MängulauaKoostaja.java
@@ -1,2 +1,39 @@
+import java.util.Random;
+
+/***
+ * Genereerib mängu alguses välja maatriksi
+ * ning lisab sinna suvalised tähed
+ * @author Maarek Vettik
+ */
+
+
 public class MängulauaKoostaja {
+    private int pikkus;
+    private int laius;
+
+    public MängulauaKoostaja(int pikkus, int laius) {
+        this.pikkus = pikkus;
+        this.laius = laius;
+    }
+
+    // Koostab sümbolmaatriksi etteantud isendiväljadega ning tagastab selle
+    public char[][] koosta() {
+        char[][] mängulaud = new char[pikkus][laius];
+
+        // Genereerib suvalise väikese tähe tähestikust, võetud StackOverflow'ist
+        // Vastendab arvu ASCII tabeli koodiga
+        Random r = new Random();
+        char sümbol = (char)(r.nextInt(26) + 'a');
+
+        // 50% võimalus, et sümbol muudetakse hoopis suureks täheks
+        int juhuarv = 1 + (int)(Math.random() * 2);
+        if (juhuarv == 1) sümbol = Character.toUpperCase(sümbol);
+
+        for (int rida = 0; rida < pikkus; rida++) {
+            for (int veerg = 0; veerg < laius; veerg++) {
+                mängulaud[rida][veerg] = sümbol;
+            }
+        }
+        return mängulaud;
+    }
 }
