250757f49ae9b119d98f661f3f642be89669729e
Maarek <maarek.vettik@ut.ee>
2026-03-31 21:33:49 +0300

    Muutsin erinevaid meetodeid ning üritasin kommenteerida

--- src/MängulauaKontrollija.java
+++ src/MängulauaKontrollija.java
@@ -1,22 +1,17 @@
 public class MängulauaKontrollija {
-
-    // Hetkel ainult proovimiseks
-    /*
-    public void main() {
-        MängulauaKoostaja m = new MängulauaKoostaja(5,5);
-        // määrab, kas ka suured ja väikesed tähed on segamini
-        // m.raskemTase();
-        char[][] maatriks = m.koosta();
-        System.out.println(kasSobib(maatriks));
-        System.out.println("---");
-
-        for (char[] maatrik : maatriks) {
-            System.out.println(maatrik);
-        }
-    }*/
-
     public boolean kasSobib(int pikkus, int laius) {
         if ((pikkus * laius) % 2 == 0) return true;
         else return false;
+    }
+
+    public boolean kasLahendatud(char[][] maatriks) {
+        for (int i = 0; i < maatriks.length; i++) {
+            for (int j = 0; j < maatriks[i].length; j++) {
+                if (maatriks[i][j] != '_') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
