535ef62d5ad82c3a56c5856477918e48909f5f5d
Maarek <maarek.vettik@ut.ee>
2026-04-29 18:50:07 +0300

    Eemaldus

--- graafiline/graafiline/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.graafiline {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.graafiline to javafx.fxml;
-    exports com.example.graafiline;
-}
