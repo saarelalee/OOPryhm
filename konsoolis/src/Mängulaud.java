e64521f67267f6810980b51ea6b3224788d7083b
Lee <lee.saarela@gmail.com>
2026-04-29 16:35:55 +0300

    graafiline ja konsool eraldi kaustades

--- konsoolis/src/Mängulaud.java
+++ konsoolis/src/Mängulaud.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 /** Haldab mängu põhiosa tegevust
--- konsoolis/src/Peaklass.java
+++ konsoolis/src/Peaklass.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Peaklass {
